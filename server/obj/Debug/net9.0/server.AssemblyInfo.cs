76949c00f14a31e4308622fa2dcbce576e72dbfe
DevExpert250802 <devkhandelwal25082002@gmail.com>
2025-04-02 15:44:30 +0530

    changes

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acb5310c31f6c10ac5181993aad16fc5a941ca2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db9ebe4dfbdd9c7b51e7c77a840e620526c981a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
